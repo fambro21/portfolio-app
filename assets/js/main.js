a2193077969147f9bb0a163bddc36b426210a574
jonathan_fambro <startek591@gmail.com>
2026-07-03 19:34:55 -0400

    refactor:navbar functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,30 @@
 (function () {
-  lucide.createIcons();
+  const menuButton = document.getElementById("menuButton");
+  const menuIcon = document.getElementById("menuIcon");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const closeMenuButton = document.getElementById("closeMenuButton");
+
+  let isOpen = false;
+
+  function renderIcon() {
+    mobileMenu.classList.toggle("open", isOpen);
+
+    menuIcon.innerHTML = isOpen
+      ? '<i data-lucide="x"></i>'
+      : '<i data-lucide="menu"></i>';
+
+    lucide.createIcons();
+  }
+
+  menuButton.addEventListener("click", () => {
+    isOpen = true;
+    renderIcon();
+  });
+
+  closeMenuButton.addEventListener("click", () => {
+    isOpen = false;
+    renderIcon();
+  });
+
+  renderIcon();
 })();
